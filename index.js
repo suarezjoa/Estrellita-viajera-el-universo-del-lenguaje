eb48b7000f9166aaebe72a02882a109b76fa0ec6
suarezjoa <gnoattosuarezjoaquin@gmail.com>
2024-04-25 19:05:44 -0300

    primera cosa

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
-});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // Enviar archivo HTML de la página de inicio
+  });
+  
+  app.get('/jugar', (req, res) => {
+    // Redireccionar a la siguiente vista (por ejemplo, 'juego.js')
+    res.redirect('/juego');
+  });
+  app.get('/admin', (req, res) => {
+    // Implementar la vista de administración (por ejemplo, mostrar un formulario o datos)
+    res.send('Página de administración');
+  });
 
 app.listen(port, () => {
   console.log(`port runing in http://localhost:${port}`);
-});
+}); 
